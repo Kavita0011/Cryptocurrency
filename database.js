22516ede78cc2d48a90de67b62dde28b18f8a11a
Kavita <kavita.codedrill@gmail.com>
2024-07-19 18:39:22 +0530

    trying to use postgres db

---
+++ database.js
@@ -0,0 +1,81 @@
+import express from "express"
+import bodyParser from "body-parser"
+import pg from "pg"
+import { database, password } from "pg/lib/defaults";
+
+const app=express();
+const port=4000;
+
+const db=new pg.Client(
+{
+    host:"localhost",
+    user:"postgres",
+    database:"crypto",
+    password:"KAVITA",
+    port:5432
+
+});
+db.connect();
+
+let quiz=[
+   { name_crypto:bitcoin_name,
+    last_price:last,
+    buy_price:buy,
+    sell_price: sell, 
+    volume_price: vol, 
+    base_unit:base_unit}
+];
+app.post("/",async(req,res) =>{
+    try{
+        console.log(req.body);
+        const type=req.body.type;
+         const Crypto=req.body.Crypto;
+        const response = await axios.get(`https://api.wazirx.com/api/v2/tickers`);
+        const result = response.data
+        ;
+        console.log(result);
+
+        for(i=1;i<=result.length;i++){
+        const bitcoin_name=Crypto;
+        const buy=response.data.ticker.buy;
+        console.log(buy)
+        const last=response.data.ticker.last;
+        const base_unit=response.data.ticker.base_unit;
+        const sell=response.data.ticker.sell;
+        const low=response.data.ticker.low;
+        const high=response.data.ticker.high;
+        const vol=response.data.ticker.vol;
+    }
+    db.query(`INSERT INTO crypto( id, name_crypto, last_price, buy_price, sell_price, volume_price, base_unit) VALUES ( ${bitcoin_name},${last},${buy}, ${sell},  ${vol}, ${base_unit})`,(err,res)=>{
+        if(err){
+            console.log(err.stack);
+        }else{
+          quiz=res.rows;  
+        }
+    console.log(`${bitcoin_name} ${sell} ${last} ${low} ${high} ${vol}`)
+
+        res.render("index.ejs",{ data:[Math.floor(Math.random() * result.length)] ,
+         });
+    }
+    catch(error){
+        console.error("failed to make request",error.message);
+        res.render("index.ejs",{ 
+            error:"No activites to match your criteria" ,
+        });
+    }
+});
+db.query('SELECT *FROM public.crypto',(err,res)=>{
+    if(err){
+        console.log(err.stack);
+    }else{
+      quiz=res.rows;  
+    }
+    
+        });
+//  INSERT INTO public.crypto(
+//             id, name_crypto, last_price, buy_price, sell_price, volume_price, base_unit)
+//             VALUES (`${bitcoin_name},${last},${buy}, ${sell},  ${vol}, ${base_unit}`);
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+});
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app=express();
 const port=4000;
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,9 +29,26 @@ app.post("/",async(req,res) =>{
         console.log(req.body);
         const type=req.body.type;
          const Crypto=req.body.Crypto;
-        const response = await axios.get(`https://api.wazirx.com/api/v2/tickers/${Crypto}`);
-        const result = response.data;
-        console.log(result);
+        const response = await axios.get(`https://api.wazirx.com/api/v2/tickers`);
+        const result = response.data
+        ;
+        // console.log(result);
+
+        for(i=1;i<=result.length;i++){
+        const bitcoin_name=Crypto;
+        const buy=response.data.ticker.buy;
+        const last=response.data.ticker.last;
+        const base_unit=response.data.ticker.base_unit;
+
+        const sell=response.data.ticker.sell;
+        const low=response.data.ticker.low;
+        const high=response.data.ticker.high;
+        const vol=response.data.ticker.vol;
+        // INSERT INTO public.crypto(
+        //     id, name_crypto, last_price, buy_price, sell_price, volume_price, base_unit)
+        //     VALUES (`${bitcoin_name},${last},${buy}, ${sell},  ${vol}, ${base_unit}`);
+    }console.log(`${bitcoin_name} ${sell} ${last} ${low} ${high} ${vol}`)
+
         res.render("index.ejs",{ data:[Math.floor(Math.random() * result.length)] ,
          });
     }
